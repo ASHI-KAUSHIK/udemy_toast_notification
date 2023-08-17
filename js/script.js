177b0990a12cdeba89681f3d565a1c16d378ef62
Ashi Kaushik <ashikaushik2109@gmail.com>
2023-08-17 20:11:06 +0530

    toast_notification

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+
+const messages = [
+    'MESSAGE ONE',
+    'MESSAGE TWO',
+    'MESSAGE THREE',
+    'MESSAGE FOUR',
+    'MESSAGE FIVE',
+    'MESSAGE SIX',
+    'MESSAGE SEVEN'
+]
+
+button.addEventListener('click', () => createNotification('This is invalid data'))
+
+function createNotification(message = null) {
+    const notif =  document.createElement('div')
+    notif.classList.add('toast')
+
+    notif.innerText = message ? message : getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    },3000)
+}
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)]
+}
